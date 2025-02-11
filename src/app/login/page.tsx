a322d7712a02817f99b1f48e30d29128e4c46416
jaeyo03 <toyazx@naver.com>
2025-02-11 16:33:07 +0900

    initial commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import {Button} from "@/components/ui/button";
+import CardWithForm from "@/components/MyCard";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <Button>
+        여기를 눌러라
+      </Button>
+      <CardWithForm/>
+    </div>
+  )
+}
